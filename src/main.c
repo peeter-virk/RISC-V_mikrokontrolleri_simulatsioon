a791b7fb9e61a45b3ba6bb03eb10f45bf0caf956
Peeter Virk <peetervirk@zohomail.eu>
2025-05-27 14:50:09 +0300

    changes

--- src/main.c
+++ src/main.c
@@ -3,14 +3,16 @@
 // Declare the external assembly function
 extern void test_arithmetic_instructions(uint32_t* result_addr);
 extern void test_csr_registers(uint32_t* result_addr);
+extern void test_memory_registers(uint32_t* result_addr);
 
 void main() {
     uint32_t arithmetic_results[56];
     uint32_t CSR_results[52];   
+    uint32_t memory_results[7];   
+
 
     // Pass the address of the first element (correct way)
+    test_arithmetic_instructions(arithmetic_results);
     test_csr_registers(arithmetic_results);
-
-    // You can now read result[0], result[1], etc.
-    // For example: if (result[0] != 8) { /* fail */ }
+    test_memory_registers(memory_results);
 }
