f47fd9336c0196aced6b7ce61d4d6298ce0a4972
Peeter Virk <peetervirk@zohomail.eu>
2025-05-29 01:46:04 +0300

    fixes and including the control transfer now.

--- src/main.c
+++ src/main.c
@@ -3,16 +3,19 @@
 // Declare the external assembly function
 extern void test_arithmetic_instructions(uint32_t* result_addr);
 extern void test_csr_registers(uint32_t* result_addr);
-extern void test_memory_registers(uint32_t* result_addr);
+extern void test_memory_instructions(uint32_t* result_addr);
+extern void test_control_transfer(uint32_t* result_addr);
 
 void main() {
     uint32_t arithmetic_results[56];
     uint32_t CSR_results[52];   
     uint32_t memory_results[7];   
+    uint32_t control_transfer_results[20];   
 
 
     // Pass the address of the first element (correct way)
     test_arithmetic_instructions(arithmetic_results);
     test_csr_registers(arithmetic_results);
-    test_memory_registers(memory_results);
+    test_memory_instructions(memory_results);
+    test_control_transfer(control_transfer_results);
 }
