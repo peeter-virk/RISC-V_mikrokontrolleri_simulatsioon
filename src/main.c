8ba9242a7d37d6301e6d4bb029db84edf0746275
Peeter Virk <peetervirk@zohomail.eu>
2025-05-29 20:41:54 +0300

    more fixes

--- src/main.c
+++ src/main.c
@@ -6,11 +6,14 @@ extern void test_csr_registers(uint32_t* result_addr);
 extern void test_memory_instructions(uint32_t* result_addr);
 extern void test_control_transfer(uint32_t* result_addr);
 
+
+uint32_t arithmetic_results[56];
+uint32_t CSR_results[52];   
+uint32_t memory_results[7];   
+uint32_t control_transfer_results[20];   
+
 void main() {
-    uint32_t arithmetic_results[56];
-    uint32_t CSR_results[52];   
-    uint32_t memory_results[7];   
-    uint32_t control_transfer_results[20];   
+    
 
 
     // Pass the address of the first element (correct way)
