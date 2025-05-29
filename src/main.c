630d3facdbb7c790b30390ae139b7acd6e5bb8b2
Peeter Virk <peetervirk@zohomail.eu>
2025-05-29 20:59:49 +0300

    tegin testikomplektide tulemusmassiivid externiks.

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@ extern void test_memory_instructions(uint32_t* result_addr);
 extern void test_control_transfer(uint32_t* result_addr);
 
 
-uint32_t arithmetic_results[56];
-uint32_t CSR_results[52];   
-uint32_t memory_results[7];   
-uint32_t control_transfer_results[20];   
+extern uint32_t arithmetic_results[56];
+extern uint32_t CSR_results[52];   
+extern uint32_t memory_results[7];   
+extern uint32_t control_transfer_results[20];   
 
 void main() {
     
